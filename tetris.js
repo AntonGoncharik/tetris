35b66e92d1ff1cd5f2176c0d96af2d45621fdd37
Anton Goncharik <ant.goncharik@gmail.com>
2019-07-03 17:23:31 +0300

    20190703 v1

--- tetris.js
+++ tetris.js
@@ -1,8 +1,16 @@
 class Game {
   constructor() {
-    this.currentFigure = [[0,0,0,0], [0,0,1,0], [1,1,1,0], [0,0,0,0,]];
+    this.currentFigure = [
+      [0, 0, 0, 0],
+      [0, 0, 1, 0],
+      [1, 1, 1, 0],
+      [0, 0, 0, 0]
+    ];
+    this.x = 120;
+    this.y = 0;
+    this.width = 20;
+    this.acceleration = 0;
   }
-
   createGame() {
     let canvasGame = document.createElement('canvas');
     canvasGame.id = 'canvasGame';
@@ -10,54 +18,77 @@ class Game {
     canvasGame.height = 500;
     document.body.appendChild(canvasGame);
     this.ctx = canvasGame.getContext('2d');
-    document.addEventListener('keydown', game.onkeydown)
+    document.addEventListener('keydown', game.onkeydown);
+    document.addEventListener('keyup', game.onkeyup);
   }
-
+  startGame() {
+    setInterval(this.moveGame.bind(this), 500);
+  }
   moveGame() {
-
+    this.renderGame();
+    if (this.acceleration) {
+      this.y += this.width * 2;
+    }
+    this.y += this.width;
   }
-
-  drawGame() {
+  renderGame() {
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-    this.ctx.fillRect(0, 0, 50, 100);
+    let x = this.x;
+    let y = this.y;
+    this.currentFigure.forEach((arrOut) => {
+      x = this.x;
+      arrOut.forEach((elm) => {
+        if (elm) this.ctx.fillRect(x, y, this.width, this.width)
+        x += this.width;
+      })
+      y += this.width;
+    })
   }
-
   rotateFigure() {
-    let arr = [[1,2,3], [4,5,6], [7,8,9]];
-    let newArr = JSON.parse(JSON.stringify(arr));
-
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr[i].length; j++) {
-        newArr[j][i] = arr[i][j];
-      }
+    let newCurrentFigure = JSON.parse(JSON.stringify(game.currentFigure));
+    game.currentFigure.forEach((arrOut, i) => {
+      arrOut.forEach((elm, j) => {
+        newCurrentFigure[j][i] = game.currentFigure[i][j];
+      })
+    })
+    game.currentFigure = newCurrentFigure.reverse();
+  }
+  shiftFigure(shift) {
+    if (shift === 'right') {
+      game.x += game.width;
+    } else {
+      game.x -= game.width;
     }
   }
-
-  shiftFigure() {
-
+  accelerationFigure() {
+    game.acceleration = 1;
   }
-
+  cancelAccelerationFigure() {
+    game.acceleration = 0;
+  }
   onkeydown(e) {
     switch (e.keyCode) {
-      case 38:  // up
-      rotateFigure();
+      case 38: // up
+        game.rotateFigure();
         break;
-      case 40:  // down
-
+      case 40: // down
+        game.accelerationFigure();
         break;
-      case 39:  // right
-      shiftFigure();
+      case 39: // right
+        game.shiftFigure('right');
         break;
-      case 37:  // left
-      shiftFigure();
+      case 37: // left
+        game.shiftFigure('left');
         break;
     }
   }
-
+  onkeyup(e) {
+    if (e.keyCode === 40) {
+      game.cancelAccelerationFigure();
+    }
+  }
 }
-
 
 let game = new Game();
 game.createGame();
-game.drawGame();
-console.log(game.currentFigure);
+game.startGame();
